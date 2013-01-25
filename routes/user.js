4a5a764534f8ac2abc56945769eafc4382118720
Reyner Crosby <reyner@gmail.com>
2013-01-25 16:53:22 -0500

    done with class

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.new = function(req, res){
+  res.send("creating a new user");
+};
